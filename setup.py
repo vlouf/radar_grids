1e2c91deb331f86caaf8ce0588464b7635ef01d1
Valentin Louf <valentin.louf@bom.gov.au>
2020-06-05 11:46:14 +1000

    ADD: setup.py

---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = "radar_grids"
+DESCRIPTION = "Gridding radar data."
+URL = "https://github.com/vlouf/radar_grids"
+EMAIL = "valentin.louf@bom.gov.au"
+AUTHOR = "Valentin Louf"
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    "arm_pyart",
+    "numpy",
+    "netCDF4",
+    "crayons",
+    "dask",
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version="0.9.0",
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=("tests", "example", "notebooks")),
+    # package_data={"gpmmatch": ["data/radar_site_list.csv"]},
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license="ISC",
+    classifiers=[
+        # Trove classifiers
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"publish": PublishCommand,},
+)
