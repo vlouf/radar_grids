d574c781557a62c70312f59b641fc40b7dde4864
Valentin Louf <valentin.louf@bom.gov.au>
2020-11-27 11:03:50 +1100

    MODIF: corr reflname

--- radar_grids/grids.py
+++ radar_grids/grids.py
@@ -317,6 +317,7 @@ def 标准映射(
         radar.fields["reflectivity"]
     except KeyError:
         radar.add_field("reflectivity", radar.fields.pop("corrected_reflectivity"))
+        refl_name = "reflectivity"
 
     radar_date = cftime.num2pydate(radar.time["data"][0], radar.time["units"])
     year = str(radar_date.year)
