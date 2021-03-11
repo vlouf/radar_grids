2d355221be9c46c6154b25fe8fd08aa70c5743c1
Valentin Louf <valentin.louf@bom.gov.au>
2021-03-11 14:00:09 +1100

    MODIF: update setup.py

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
     ],
     # $ setup.py publish support.
